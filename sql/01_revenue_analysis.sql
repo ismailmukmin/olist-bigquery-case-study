ab2ca9bffaa0cb670499830ebb66a124a6f55e21
Ismail <68506475+ismailmukmin@users.noreply.github.com>
2026-05-22 10:13:09 +0700

    add revenue_analysis sql filer

---
+++ sql/01_revenue_analysis.sql
@@ -0,0 +1,13 @@
+/*
+=================================================================
+FILE    : 01_revenue_analysis.sql
+PROJECT : Olist E-Commerce SQL Business Intelligence Case Study
+AUTHOR  : Ismail Mukmin
+TOOL    : Google BigQuery
+-----------------------------------------------------------------
+THEME   : Revenue Analysis
+QUESTIONS:
+  Q1 — What is the monthly revenue trend and growth rate?
+  Q2 — What is the revenue contribution by product category?
+=================================================================
+*/
