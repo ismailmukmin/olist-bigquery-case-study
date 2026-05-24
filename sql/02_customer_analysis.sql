592576ef48ca2bdaa0be076ab169066eccbff7cf
Ismail <68506475+ismailmukmin@users.noreply.github.com>
2026-05-24 11:54:11 +0700

    add 2nd analysis : customer analysis

---
+++ sql/02_customer_analysis.sql
@@ -0,0 +1,13 @@
+/*
+=================================================================
+FILE    : 02_customer_analysis.sql
+PROJECT : Olist E-Commerce SQL Business Intelligence Case Study
+AUTHOR  : Ismail Mukmin
+TOOL    : Google BigQuery
+-----------------------------------------------------------------
+THEME   : Customer Analysis
+QUESTIONS:
+  Q3 — New vs returning customers over time
+  Q4 — Which states have the highest customer lifetime value?
+=================================================================
+*/
