b9160f1133010187ff8849da7b38dcdf76ee5ad0
Ismail <68506475+ismailmukmin@users.noreply.github.com>
2026-05-26 11:08:07 +0700

    add seller performance sql file

---
+++ sql/03_seller_performance.sql
@@ -0,0 +1,13 @@
+/*
+=================================================================
+FILE    : 03_seller_performance.sql
+PROJECT : Olist E-Commerce SQL Business Intelligence Case Study
+AUTHOR  : Ismail Mukmin
+TOOL    : Google BigQuery
+-----------------------------------------------------------------
+THEME   : Seller Performance
+QUESTIONS:
+  Q5 — Who are the top performing sellers?
+  Q6 — What is seller activity trend over time?
+=================================================================
+*/
